bc5e7632279e83c096d12960a927dc62b901bb3f
bo-co <ulyanoff@iMac.local>
2019-02-15 21:13:23 +0300

    [ footer ][ contacts ]

--- js/template.js
+++ js/template.js
@@ -1,13 +1,32 @@
 var myMap;
 var routePanelControl;
+var myPlacemark;
+var multiRoutePromise;
 ymaps.ready(init);
 
 function init() {
     myMap = new ymaps.Map("map", {
-        center: [55.75164123543684, 37.662869272700114],
+        center: [55.74964123543684, 37.662869272700114],
         zoom: 15,
         controls: []
-    	});    	
+    	});
+	myMap.controls.add("zoomControl", {
+    	size: "small"
+		});
+	myPlacemark = new ymaps.Placemark([55.75164123543684, 37.662869272700114], {
+		balloonContent: "«Дельтаклиник»<br />Москва, Наставнический перeулок, 6"
+		}, {
+    	iconLayout: "default#image",
+    	iconImageHref: "img/finish.svg",
+    	iconImageSize: [40, 40],
+    	iconImageOffset: [-20, -20]
+		});		
+	myMap.geoObjects.add(myPlacemark);
+	myMap.behaviors.disable("scrollZoom");	
+    }
+    
+function map() {
+	myMap.geoObjects.remove(myPlacemark);		
     routePanelControl = new ymaps.control.RoutePanel({
         options: {
             showHeader: true,
@@ -17,30 +36,35 @@ function init() {
             	masstransit: false,
             	auto: false
             	},
-            maxWidth: "280px",
-            visible: false
+            maxWidth: "260px",
+            visible: false,
+            float: "none",
+            position: {
+            	top : 20,
+            	right : 20
+            	}
        		}
     	});    	
     routePanelControl.routePanel.state.set({
     	type: "auto",
-        to: "Москва, Наставнический пер., 6",
+        to: [55.75164123543684, 37.662869272700114],
         toEnabled: false
     	});
 	routePanelControl.routePanel.options.set({
     	allowSwitch: false,
         reverseGeocoding: true
     	});    	
-    var multiRoutePromise = routePanelControl.routePanel.getRouteAsync();    
+    multiRoutePromise = routePanelControl.routePanel.getRouteAsync();    
     multiRoutePromise.then(function(multiRoute) {
     	multiRoute.options.set({
     		wayPointStartIconLayout: "default#image",
     		wayPointStartIconImageHref: "img/start.svg",
-    		wayPointStartIconImageSize: [50, 50],
-    		wayPointStartIconImageOffset: [-25, -25],
+    		wayPointStartIconImageSize: [40, 40],
+    		wayPointStartIconImageOffset: [-20, -20],
    			wayPointFinishIconLayout: "default#image",
     		wayPointFinishIconImageHref: "img/finish.svg",
-    		wayPointFinishIconImageSize: [50, 50],
-    		wayPointFinishIconImageOffset: [-25, -25]
+    		wayPointFinishIconImageSize: [40, 40],
+    		wayPointFinishIconImageOffset: [-20, -20]
   			})
     	});
 	myMap.controls.add(routePanelControl);
@@ -48,6 +72,9 @@ function init() {
 	
 $(document).ready(function() {
 	$("div.container > footer > div.contacts > div > ul > li > a.route").on("click", function(e) {
+		if(!routePanelControl) {
+			map();
+			}
 		routePanelControl.routePanel.state.set({
 			type: $(this).data("type")
 			});			
