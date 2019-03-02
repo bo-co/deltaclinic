745b0abd2e32d8de78a14c6246d867140467369a
bo-co <ulyanoff@iMac.local>
2019-03-02 22:03:09 +0300

    [ map ][ update ]

--- js/map.js
+++ js/map.js
@@ -1,71 +1,50 @@
-ymaps.ready(init);
-
 function init() {
-	
+
 	var routePanelControl,
 		multiRoutePromise,
 		myPlacemark = new ymaps.Placemark([55.75164123543684, 37.662869272700114], {
-		balloonContent: "«Дельтаклиник»<br />Москва, Наставнический перeулок, 6"
-		}, {
-    	iconLayout: "default#image",
-    	iconImageHref: "img/finish.svg",
-    	iconImageSize: [40, 40],
-    	iconImageOffset: [-20, -20]
-		});
-		
-	function map() {
-		myMap.geoObjects.remove(myPlacemark);		
-    	routePanelControl = new ymaps.control.RoutePanel({
-        	options: {
-            	showHeader: true,
-            	title: "Маршруты",
-            	routePanelTypes: {
-            		pedestrian: false,
-            		masstransit: false,
-            		bicycle: false,
-            		auto: false,
-            		taxi: false
-            		},
-            	maxWidth: "260px",
-            	visible: false,
-            	float: "none",
+			balloonContent: null,
+			hintContent: null
+			}, {
+    		iconLayout: 'default#image',
+    		iconImageHref: 'img/finish.svg',
+    		iconImageSize: [40, 40],
+    		iconImageOffset: [-20, -20]
+			}),
+		multiRouteModel = new ymaps.multiRouter.MultiRouteModel([], {
+            wayPointDraggable: false,
+            boundsAutoApply: false
+        	}),
+        routeTypeSelector = new ymaps.control.ListBox({
+            data: {
+                content: 'Как добраться'
+            	},
+            items: [
+                new ymaps.control.ListBoxItem({data: {content: 'Авто'}}),
+                new ymaps.control.ListBoxItem({data: {content: 'Пешком'}})
+            	],
+            options: {
+                itemSelectOnClick: false,
+                float: 'none',
             	position: {
             		top : 20,
             		right : 20
-            		}
-       			}
-    		});    	
-    	routePanelControl.routePanel.state.set({
-    		type: "auto",
-        	to: "Наставнический перeулок, 6",
-        	toEnabled: false
-    		});
-		routePanelControl.routePanel.options.set({
-    		allowSwitch: false,
-        	reverseGeocoding: true
-    		});    	
-    	multiRoutePromise = routePanelControl.routePanel.getRouteAsync();    
-    	multiRoutePromise.then(function(multiRoute) {
-    		multiRoute.options.set({
-    			wayPointStartIconLayout: "default#image",
-    			wayPointStartIconImageHref: "img/start.svg",
-    			wayPointStartIconImageSize: [40, 40],
-    			wayPointStartIconImageOffset: [-20, -20],
-   				wayPointFinishIconLayout: "default#image",
-    			wayPointFinishIconImageHref: "img/finish.svg",
-    			wayPointFinishIconImageSize: [40, 40],
-    			wayPointFinishIconImageOffset: [-20, -20]
-  				})
-    		});
-		myMap.controls.add(routePanelControl);
-		routePanelControl.routePanel.geolocate("from");
-		}
+            		},
+            	visible: false
+            	}
+        	}),
+        autoRouteItem = routeTypeSelector.get(0),
+        pedestrianRouteItem = routeTypeSelector.get(1);        
+    	autoRouteItem.events.add('click', function(e) { changeRoutingMode('auto', e.get('target')); });
+    	pedestrianRouteItem.events.add('click', function(e) { changeRoutingMode('pedestrian', e.get('target')); });
 		
     myMap = new ymaps.Map('map', {
         center: [55.74964123543684, 37.662869272700114],
         zoom: 15,
-        controls: []
-    	}),
+        controls: [routeTypeSelector]
+    	}, {
+        buttonMaxWidth: 300
+        }),
     ZoomLayout = ymaps.templateLayoutFactory.createClass('<div class="zoom"><div class="in" id="in"></div><div class="out" id="out"></div></div>', {
         build: function () {
         	ZoomLayout.superclass.build.call(this);
@@ -98,71 +77,56 @@ function init() {
     	options: {
     		layout: ZoomLayout
     		}
-    	});
+    	}),
+    multiRoute = new ymaps.multiRouter.MultiRoute(multiRouteModel, {
+        wayPointDraggable: false,
+        boundsAutoApply: false
+        });
     myMap.controls.add(zoomControl);
     myMap.behaviors.disable('scrollZoom');
     myMap.geoObjects.add(myPlacemark);
+    
+	multiRoute.options.set({
+		wayPointStartIconLayout: 'default#image',
+		wayPointStartIconImageHref: 'img/start.svg',
+    	wayPointStartIconImageSize: [40, 40],
+    	wayPointStartIconImageOffset: [-20, -20],
+   		wayPointFinishIconLayout: 'default#image',
+    	wayPointFinishIconImageHref: 'img/finish.svg',
+    	wayPointFinishIconImageSize: [40, 40],
+    	wayPointFinishIconImageOffset: [-20, -20]
+  		});
+    
+	ymaps.geolocation.get({
+		provider: 'auto',
+		autoReverseGeocode: false,
+		mapStateAutoApply: true
+		}).then(function (result) {
+		multiRouteModel.setReferencePoints([result.geoObjects.get(0).geometry.getCoordinates(), 'Москва, Наставнический перeулок, 6']);
+		});
+    
+    function changeRoutingMode(routingMode, targetItem) {
+    	if (myPlacemark) {
+    		myMap.geoObjects.remove(myPlacemark);
+    		}
+        multiRouteModel.setParams({ routingMode: routingMode }, true);
+        autoRouteItem.deselect();
+        pedestrianRouteItem.deselect();
+        targetItem.select();
+        routeTypeSelector.collapse();
+        myMap.geoObjects.add(multiRoute);
+        myMap.setBounds(multiRoute.getBounds());
+    	}
 
-	$("div.container > footer > div.contacts > div > ul > li > a.route").on("click", function(e) {
-		myMap.geoObjects.remove(myPlacemark);		
-    	routePanelControl = new ymaps.control.RoutePanel({
-        	options: {
-            	showHeader: true,
-            	title: $(this).data("title"),
-            	routePanelTypes: {
-            		pedestrian: false,
-            		masstransit: false,
-            		bicycle: false,
-            		auto: false,
-            		taxi: false
-            		},
-            	maxWidth: "260px",
-            	visible: true,
-            	float: "none",
-            	position: {
-            		top : 20,
-            		right : 20
-            		}
-       			}
-    		});
-		routePanelControl.routePanel.geolocate("from");    	
-    	routePanelControl.routePanel.state.set({
-    		type: $(this).data("type"),
-        	to: "Наставнический перeулок, 6",
-        	toEnabled: false
-    		});
-		routePanelControl.routePanel.options.set({
-    		allowSwitch: false,
-        	reverseGeocoding: true
-    		}); 	
-    	multiRoutePromise = routePanelControl.routePanel.getRouteAsync();    
-    	multiRoutePromise.then(function(multiRoute) {
-    		multiRoute.options.set({
-    			wayPointStartIconLayout: "default#image",
-    			wayPointStartIconImageHref: "img/start.svg",
-    			wayPointStartIconImageSize: [40, 40],
-    			wayPointStartIconImageOffset: [-20, -20],
-   				wayPointFinishIconLayout: "default#image",
-    			wayPointFinishIconImageHref: "img/finish.svg",
-    			wayPointFinishIconImageSize: [40, 40],
-    			wayPointFinishIconImageOffset: [-20, -20]
-  				})
-    		});
-		myMap.controls.add(routePanelControl);
-		setTimeout(function() {
-			$(".ymaps-2-1-72-route-panel-input__icon_first .ymaps-2-1-72-route-panel-input__circle").css("fill", "rgb(1, 31, 88)");	
-			$(".ymaps-2-1-72-route-panel-input__icon_last .ymaps-2-1-72-route-panel-input__circle").css("fill", "rgb(244, 86, 65)");
-			}, 100);
+	$('div.container > footer > div.contacts > div > ul > li > a.route').on('click', function(e) {
+		if ($(this).data('type') === 'pedestrian') {
+			changeRoutingMode($(this).data('type'), pedestrianRouteItem);
+			}
+		else {
+			changeRoutingMode($(this).data('type'), autoRouteItem);
+			}
 		e.preventDefault();
 		});
-		
-	$(document).mouseup(function(e) {
-		if(!$("div#map").is(e.target) && $("div#map").has(e.target).length === 0 && $("div#map").is(":visible")) {
-			if(routePanelControl) {
-				myMap.controls.remove(routePanelControl);
-				myMap.geoObjects.add(myPlacemark);
-				}
-			}
-    	e.preventDefault();
-		});
     }
+    
+ymaps.ready(init);
